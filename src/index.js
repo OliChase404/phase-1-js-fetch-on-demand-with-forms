99c0cced5f8bc780c20c796322e682c1dd5abe92
Oliver <olichase91@gmail.com>
2023-02-28 09:04:27 -0800

    Solved Lab'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-const init = () => {
-  
-}
+const initialise = () => {
 
-document.addEventListener('DOMContentLoaded', init);
+const inputForm = document.getElementById('form')
+const inputValue = document.getElementById('searchByID')
+const titleSection = document.getElementById('titleSection')
+const summarySection = document.getElementById('summarySection')
+const newTitleSection = document.createElement('p')
+const newSummarySection = document.createElement('p')
+titleSection.appendChild(newTitleSection)
+summarySection.appendChild(newSummarySection)
+
+inputForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    getMovie(inputValue.value)
+})
+    
+    
+function getMovie(id){
+    fetch(`http://localhost:3000/movies/${id}`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    newTitleSection.textContent = data.title
+    newSummarySection.textContent = data.summary
+  })
+  .catch((error) => {console.error('Fetch Error:', error)})
+}
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+}
+document.addEventListener('DOMContentLoaded', initialise);
